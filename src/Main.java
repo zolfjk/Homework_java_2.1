abe185897bb7b95d65a364de06b9240ff03b2265
zolfjk <zolfjkible@gmail.com>
2022-04-23 18:42:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int priceForBonus = 20;
+
+        int bonusMile = ticketPrice / priceForBonus;
+
+        System.out.println("Количество бонусных миль: " + bonusMile);
+
+
+    }
+
+}
